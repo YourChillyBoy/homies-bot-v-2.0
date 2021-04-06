483c332c892b86aefa1e0609cbcc2da25f04e553
YourChillyBoy <82034647+YourChillyBoy@users.noreply.github.com>
2021-04-06 16:58:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const { CommandoClient } = require('discord.js-commando');
+const { Structures, MessageEmbed, MessageAttachment } = require('discord.js');
+const path = require('path');
+const { prefix, token, discord_owner_id } = require('./config.json');
+const db = require('quick.db');
+const Canvas = require('canvas');
+
+const client = new Client();//Making a discord bot client
+client.commands = new Collection();//Making client.commands as a Discord.js Collection
+client.queue = new Map()
+
+client.config = {
+  prefix: process.env.PREFIX,
+  SOUNDCLOUD: process.env.SOUNDCLOUD_CLIENT_ID
+}
+
+//Loading Events
+fs.readdir(__dirname + "/events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(__dirname + `/events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+    console.log("Loading Event: "+eventName)
+  });
+});
+
+//Loading Commands
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+    console.log("Loading Command: "+commandName)
+  });
+});
+
+//Logging in to discord
+client.login(process.env.TOKEN)
